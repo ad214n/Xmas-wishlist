05fe3243bdbf2a097ed3429dbf1a2cdf05c1c3e2
Adrian Altares <adrianaltares@Adrians-MBP.lan>
2018-11-19 12:17:04 -0500

    child controller

--- controllers/childController.js
+++ controllers/childController.js
@@ -9,12 +9,24 @@ const childController = {
             })
     },
     create: (req, res) => {
-        // take req.params and req.body input inputs and format for DB
-
-        // send formatted inputs to DB
-
-        // send res back
-    },
+     // take req.params and req.body input inputs and format for DB
+     // send formatted inputs to DB
+     // send res back
+        var childId = req.params.userId
+        Child.findById(userId)
+            .then((child) => {
+                console.log(child)
+                Idea.create(req.body)
+                    .then((child) => {
+                        console.log(newChild)
+                        user.ideas.push(newChild)
+                        user.save()
+                        res.send(newChild)
+                    })
+                })
+        }
+    
+    }
     // pulls back a single child
     retrieve: (req, res) => {
         var childId = req.params.childId
@@ -25,18 +37,27 @@ const childController = {
     },
     update: (req, res) => {
         // get the req.params and req.body input inputs and format for DB
-
         // update the child from the DB by sending the params in
-
         // send res back
+        var ideaId = req.params.ideaId
+        Idea.findByIdAndUpdate(ideaId, req.body, { new: true })
+            .then((updatedIdea) => {
+                updatedIdea.save()
+                res.send(updatedIdea)
+            })
     },
     delete: (req, res) => {
+        var ideaId = req.params.childId
+        Child.findByIdAndDelete(childId)
+            .then(() => {
+                res.send(200)
+            })
+    },
         // get the child ID from the req.params
 
         // delete the child from the DB
 
         // send res back
-    },
-}
+  
 
 module.exports = childController
