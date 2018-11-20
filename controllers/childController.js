6c0aadf24d526cc7aec2d78f312ac7ba1d40fa7b
Adrian Altares <adrianaltares@Adrians-MBP.lan>
2018-11-19 19:44:57 -0500

    modified controllers

--- controllers/childController.js
+++ controllers/childController.js
@@ -1,4 +1,4 @@
-const Child = require('../models/Child')
+const Child = require('../models/Child');
 
 const childController = {
     // pulls back all children
@@ -12,53 +12,37 @@ const childController = {
         // take req.params and req.body input inputs and format for DB
         // send formatted inputs to DB
         // send res back
-        var parentId = req.params.id
-        Parent.findById(parentId)
-            .then((parent) => {
-                console.log(parent)
-                Child.create(req.body)
-                    .then((newChild) => {
-                        console.log(newChild)
-                        parent.children.push(newChild)
-                        parent.save()
-                        res.send(newChild)
-                    })
-            })
+        Child.create(req.body)
+            .then((newChild) => {
+                res.send(newChild);
+            });
     },
-
-
     // pulls back a single child
     retrieve: (req, res) => {
     var childId = req.params.childId
-Child.findById(childId)
-    .then((child) => {
-        res.send(child)
-    })
-},
-update: (req, res) => {
-    // get the req.params and req.body input inputs and format for DB
-    // update the child from the DB by sending the params in
-    // send res back
-    var ideaId = req.params.ideaId
-    Idea.findByIdAndUpdate(ideaId, req.body, { new: true })
-        .then((updatedIdea) => {
-            updatedIdea.save()
-            res.send(updatedIdea)
-        })
-},
+    Child.findById(childId)
+        .then((child) => {
+            res.send(child);
+        });
+    },
+    update: (req, res) => {
+        // get the req.params and req.body input inputs and format for DB
+        // update the child from the DB by sending the params in
+        // send res back
+        var childId = req.params.id
+        Child.findByIdAndUpdate(childId, req.body, { new: true })
+            .then((updatedChild) => {
+                updatedChild.save();
+                res.send(updatedChild);
+            });
+    },
     delete: (req, res) => {
-        var ideaId = req.params.childId
+        var childId = req.params.id
         Child.findByIdAndDelete(childId)
             .then(() => {
-                res.send(200)
+                res.send(200);
             })
     }
 }
-        // get the child ID from the req.params
 
-        // delete the child from the DB
-
-        // send res back
-
-
-        module.exports = childController
+module.exports = childController
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,4 +1,5 @@
-const Item = require('../models/Item')
+const Item = require('../models/Item');
+const Child = require('../models/Child');
 
 const itemController = {
     // pulls back all items
@@ -9,53 +10,48 @@ const itemController = {
             })
     },
     create: (req, res) => {
-        var itemId = req.params.itemId
-        Item.findById(itemId)
-            .then((item) => {
-                console.log(item)
-                Idea.create(req.body)
+        var childId = req.params.id
+        Child.findById(childId)
+            .then((child) => {
+                console.log(child);
+                Item.create(req.body)
                     .then((newItem) => {
-                        console.log(newItem)
-                        item.ideas.push(newItem)
-                        item.save()
-                        res.send(newItem)
-                    })
-            })
+                        console.log(newItem);
+                        child.ideas.push(newItem);
+                        child.save();
+                        res.send(newItem);
+                    });
+            });
     },
-
-
     // pulls back a single item
     retrieve: (req, res) => {
-        var itemId = req.params.itemId
+        var itemId = req.params.id
         Item.findById(itemId)
             .then((item) => {
-                res.send(item)
+                res.send(item);
             })
     },
-    
-// get the req.params and req.body input inputs and format for DB
-// update the item from the DB by sending the params in
-// send res back
-update: (req, res) => {
-    var itemId = req.params.itemId
-    Item.findByIdAndUpdate(ItemId, req.body, { new: true })
-        .then((updatedItem) => {
-            updatedItem.save()
-            res.send(updatedItem)
-        })
-},
-
-    
-// get the item ID from the req.params
-// delete the item from the DB
-// send res back
-delete: (req, res) => {
-    var itemId = req.params.itemId
-    Item.findByIdAndDelete(itemId)
-        .then(() => {
-            res.send(200)
-        })
-}
+    // get the req.params and req.body input inputs and format for DB
+    // update the item from the DB by sending the params in
+    // send res back
+    update: (req, res) => {
+        var itemId = req.params.id
+        Item.findByIdAndUpdate(itemId, req.body, { new: true })
+            .then((updatedItem) => {
+                updatedItem.save();
+                res.send(updatedItem);
+            });
+    },
+    // get the item ID from the req.params
+    // delete the item from the DB
+    // send res back
+    delete: (req, res) => {
+        var itemId = req.params.id
+        Item.findByIdAndDelete(itemId)
+            .then(() => {
+                res.send(200);
+            });
+    }
 }
 
 module.exports = itemController
